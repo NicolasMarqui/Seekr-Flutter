d78cfb136a75a6fc46bac5b3553c8a8ee687ea29
Nicolas <nicolas.marqui@fatec.sp.gov.br>
2019-12-27 15:20:59 -0300

    First Commit

---
+++ frontend/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import './pages/home.dart';
+
+void main() => runApp(StartApp());
+
+class StartApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Seekr",
+      home: HomePage(),
+    );
+  }
+
+}
